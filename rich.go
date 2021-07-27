2e4b1f0c97d71b29d14eb61609c414fdf621732f
michaelskyba <michaelskyba1411@gmail.com>
2021-07-27 02:22:06 -0400

    decide on rich home directory

--- rich.go
+++ rich.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Welcome to the Rich Habit Tracker.")
+	var home_dir string
+	if os.Getenv("RICH_HOME") == "" {
+		home_dir = fmt.Sprintf("%v/.local/share/rich", os.Getenv("HOME"))
+	} else {
+		home_dir = os.Getenv("RICH_HOME")
+	}
+
+	fmt.Println(home_dir)
 }
