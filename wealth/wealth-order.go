a732070d6468126aa8838addcb9bf26e6fac2fa1
Michael Skyba <michaelskyba@net-c.com>
2022-07-21 23:53:50 -0400

    Create map[string]bool from ordered habits

--- wealth/wealth-order.go
+++ wealth/wealth-order.go
@@ -28,6 +28,25 @@ func getLines(filename string) []string {
 	return lines
 }
 
+/*
+With a hash table, we can check for duplicates in O(1) time instead of O(r)
+time(where r is the size of the ordered input). This means that the unique
+extraction process will take O(h) time instead of O(hr) time (where h is the
+size of the full habits list file). Since we're already going to be using O(r)
+space with the list of lines, the space would be O(2r) which is shortened to
+O(r). Basically, the use of a hash table makes the program significantly faster
+at high inputs without using much extra memory.
+*/
+func createHashTable(lines []string) map[string]bool {
+	seen := map[string]bool{}
+
+	for _, line := range lines {
+		seen[line] = true
+	}
+
+	return seen
+}
+
 func main() {
 	filename1 := os.Args[1]
 	filename2 := os.Args[2]
@@ -35,5 +54,8 @@ func main() {
 	fmt.Println(filename1, filename2)
 
 	ordered := getLines(filename2)
+	orderedHash := createHashTable(ordered)
+
 	fmt.Println(ordered)
+	fmt.Println(orderedHash)
 }
