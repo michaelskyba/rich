1a194cfefff692e431d2433a0e68ca3d1f2c4839
Michael Skyba <michaelskyba@net-c.com>
2022-07-21 23:47:29 -0400

    Create []string from ordered habits

--- wealth/wealth-order.go
+++ wealth/wealth-order.go
@@ -5,11 +5,35 @@ package main
 import (
 	"fmt"
 	"os"
+	"bufio"
 )
+
+func hdl(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func getLines(filename string) []string {
+	file, err := os.Open(filename)
+	hdl(err)
+
+	scanner := bufio.NewScanner(file)
+	lines := []string{}
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	return lines
+}
 
 func main() {
 	filename1 := os.Args[1]
 	filename2 := os.Args[2]
 
 	fmt.Println(filename1, filename2)
+
+	ordered := getLines(filename2)
+	fmt.Println(ordered)
 }
